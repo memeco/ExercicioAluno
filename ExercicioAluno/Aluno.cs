900a05722c5d3da4b28747d78d19ebdf2769800f
Emerson Marques Pedro <emersonmp@gmail.com>
2024-03-09 13:09:59 -0300

    Adicionei a classe Aluno.cs

---
+++ ExercicioAluno/Aluno.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExercicioAluno
+{
+    public class Aluno
+    {
+        // Atributos da classe
+        public string Nome { get; set; }
+        public double NotaPrimeiroTrimestre { get; set; }
+        public double NotaSegundoTrimestre { get; set; }
+        public double NotaTerceiroTrimestre { get; set; }
+
+        // Construtor da classe
+        public Aluno(string nome, double notaPrimeiroTrimestre, double notaSegundoTrimestre, double notaTerceiroTrimestre)
+        {
+            Nome = nome;
+            NotaPrimeiroTrimestre = notaPrimeiroTrimestre;
+            NotaSegundoTrimestre = notaSegundoTrimestre;
+            NotaTerceiroTrimestre = notaTerceiroTrimestre;
+        }
+
+        // Métodos da classe
+        public double CalcularMedia()
+        {
+            return (NotaPrimeiroTrimestre + NotaSegundoTrimestre + NotaTerceiroTrimestre) / 3;
+        }
+
+        public bool EstaAprovado()
+        {
+            return CalcularMedia() >= 6;
+        }
+
+        public string ObterSituacao()
+        {
+            if (EstaAprovado())
+            {
+                return "APROVADO";
+            }
+            else
+            {
+                return "REPROVADO";
+            }
+        }
+
+        public double PontosFaltantes()
+        {
+            return 6 - CalcularMedia();
+        }
+    }
+}
